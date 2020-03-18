beb78bfef5dcdb52e7aae7bae0d1a824fc766e29
Sergei Ugdyzhekov <sergey.ugdyzhekov@jetbrains.com>
2020-03-18 21:48:25 +0100

    fix: The value of 'access_logs.s3.bucket' cannot be empty

--- alb.tf
+++ alb.tf
@@ -3,10 +3,13 @@ resource "aws_alb" "ingress" {
   security_groups = [aws_security_group.alb.id]
   subnets         = local.public_subnets
 
-  access_logs {
-    bucket  = local.access_logs_bucket
-    prefix  = local.access_logs_prefix
-    enabled = local.access_logs_enable
+  dynamic "access_logs" {
+    for_each = local.access_logs_enable ? [""] : []
+    content {
+      bucket  = local.access_logs_bucket
+      prefix  = local.access_logs_prefix
+      enabled = local.access_logs_enable
+    }
   }
 
   tags = {
