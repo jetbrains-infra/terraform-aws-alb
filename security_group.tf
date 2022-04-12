e142c7537784d533ea742ab063dfe5022044b917
Patrick Kostas <patrick.kostas@jetbrains.com>
2022-04-12 11:37:47 +0200

    allow ingress across whole vpc in case of internal alb

--- security_group.tf
+++ security_group.tf
@@ -4,6 +4,10 @@ resource "aws_security_group" "alb" {
   tags   = local.tags
 }
 
+data "aws_vpc" "vpc" {
+  id = local.vpc_id
+}
+
 resource "aws_security_group_rule" "lb_allow_outgoing_connection_to_private_subnets" {
   protocol          = "-1"
   from_port         = "0"
@@ -13,12 +17,14 @@ resource "aws_security_group_rule" "lb_allow_outgoing_connection_to_private_subn
   cidr_blocks       = local.target_cidr_blocks
 }
 
-resource "aws_security_group_rule" "lb_allow_incoming_connection_from_internet" {
+resource "aws_security_group_rule" "lb_allow_incoming_connection_from_internet_or_vpc_only" {
   count             = length(local.all_ports)
   protocol          = "TCP"
   from_port         = element(local.all_ports, count.index)
   to_port           = element(local.all_ports, count.index)
   security_group_id = aws_security_group.alb.id
   type              = "ingress"
-  cidr_blocks       = local.internal ? local.target_cidr_blocks : ["0.0.0.0/0"]
+  cidr_blocks = local.internal ? [for a in data.aws_vpc.vpc.cidr_block_associations : a.cidr_block] : [
+    "0.0.0.0/0"
+  ]
 }
